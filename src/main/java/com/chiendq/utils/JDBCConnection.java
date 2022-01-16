670590639fdb9138d2fccd89c143ac48558ceecf
chiendq <chiendq.work@gmail.com>
2022-01-16 21:35:43 +0700

    Use ResourceBundle to separate Connection config

--- src/main/java/com/chiendq/utils/JDBCConnection.java
+++ src/main/java/com/chiendq/utils/JDBCConnection.java
@@ -3,6 +3,7 @@ package com.chiendq.utils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class JDBCConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/simple_todo_app";
@@ -10,12 +11,18 @@ public class JDBCConnection {
     private static final String password = "";
     private static Connection connection;
 
+    static ResourceBundle resourceBundle = ResourceBundle.getBundle("db");
+
     public JDBCConnection() {
     }
 
     public static Connection getConnection()  {
         try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(resourceBundle.getString("driverName"));
+            String url = resourceBundle.getString("url");
+            String user = resourceBundle.getString("user");
+            String password = resourceBundle.getString("password");
             connection = DriverManager.getConnection(URL, user, password);
         }catch (Exception e){
             e.printStackTrace();
